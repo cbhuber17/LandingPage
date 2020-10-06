775ef6565cd76a17678c6e8b16de35cf1a21badb
Colin Huber <cbhuber@gmail.com>
2020-10-05 21:33:31 -0600

    Updated index.html to have header out of main for fixed navbar.  Added CSS for the fixed navbar.  Further JS coding for active viewport.

--- js/app.js
+++ js/app.js
@@ -24,12 +24,42 @@ addNavBarList(navBarList, 'Section 2', '#section2');
 addNavBarList(navBarList, 'Section 3', '#section3');
 addNavBarList(navBarList, 'Section 4', '#section4');
 
-// TODO: Smooth scrolling to section
-// section.scrollIntoView({behavior: 'smooth'});
+// Smooth scrolling to a section
+section2 = document.querySelector('#section2');
+section2anchor = document.querySelector('a[href ="#section2"]');
 
-// document.querySelector(this.getAttribute('href')).scrollIntoView({
-//     behavior: 'smooth'
-// });
-
+section2anchor.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    section2.scrollIntoView({ behavior: 'smooth' });
+})
 
 console.log(navBarList);
+
+
+
+// element is the dom element, i.e. what's returned from document.querySelector('selector')
+
+// function isOnScreen(element, buffer) {
+//     //buffer is optional and allows you to return true when  
+//     //the element is going to appear to the screen  
+//     buffer = typeof buffer === 'undefined' ? 0 : buffer;
+//     // Get element's position in the viewport
+//     const bounding = element.getBoundingClientRect();
+//     console.log(bounding);
+//     console.log(window.innerWidth);
+//     console.log(window.innerHeight);
+
+//     // Check if element is in the viewport 
+//     if (bounding.top >= buffer &&
+//         bounding.left >= buffer &&
+//         bounding.right <= (window.innerWidth - buffer) &&
+//         bounding.bottom <= (window.innerHeight - buffer)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// section2 = document.querySelector('header');
+// isSec2OnScreen = isOnScreen(section2, 0);
+// console.log(isSec2OnScreen);
